9b8f94a5d202f55a856eaa5488cd2ec849a97f8d
josypinho75@gmail.com <josypinho75@gmail.com@74d0a7c4-93f3-1cb1-9434-061c075ea34c>
2013-12-01 01:43:53 +0000

    Commit inicial

---
+++ trunk/QuestionariosPedagogia/src/ifrn/etep/dominio/RepositorioDiretoriaAcademica.java
@@ -0,0 +1,15 @@
+package ifrn.etep.dominio;
+
+import java.util.List;
+
+public interface RepositorioDiretoriaAcademica {
+	
+	void insert(DiretoriaAcademica diretoria);
+	
+	void delete(Integer idDiretoria);
+	
+	void update(DiretoriaAcademica diretoria);
+	
+	List<DiretoriaAcademica> getAll();
+
+}
